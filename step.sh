561806f082916450298d1ba6b468e03de19d39dc
Anthony Dade <145477058+AnthonyDadeWT@users.noreply.github.com>
2023-12-12 10:55:15 -0500

    Updating inputs

--- step.sh
+++ step.sh
@@ -21,6 +21,42 @@ echo "This is the value specified for the input 'example_step_input': ${example_
 #  with a 0 exit code `bitrise` will register your Step as "successful".
 # Any non zero exit code will be registered as "failed" by `bitrise`.
 
+RESTORE='\033[0m'
+RED='\033[00;31m'
+YELLOW='\033[00;33m'
+BLUE='\033[00;34m'
+GREEN='\033[00;32m'
+
+function color_echo {
+    color=$1
+    msg=$2
+    echo -e "${color}${msg}${RESTORE}"
+}
+
+function echo_fail {
+    msg=$1
+    echo
+    color_echo "${RED}" "${msg}"
+    exit 1
+}
+
+function echo_warn {
+    msg=$1
+    color_echo "${YELLOW}" "${msg}"
+}
+
+function echo_info {
+    msg=$1
+    echo
+    color_echo "${BLUE}" "${msg}"
+}
+
+function echo_details {
+    msg=$1
+    echo "  ${msg}"
+}
+
+
 
 
 #=======================================
@@ -33,21 +69,20 @@ echo_details "* service_credentials_file_path: $service_account_credentials_file
 echo_details "* test_apk_path: $test_apk_path"
 echo_details "* app_apk_path: $apk_path"
 echo_details "* project: $project_id"
-
-echo
+echo_details "* build_flavor: $build_flavor"
 
 # Checking regular APK
 if [ -z "${apk_path}" ] ; then
-    echo_fail "App path for APK, AAB or IPA is not defined"
+    echo_fail "The path for APK, AAB or IPA is not defined"
 fi
 
 case "${apk_path}" in
     \|\|*)
-       echo_warn "App path starts with || . Manually fixing path: ${apk_path}"
+       echo_warn "The app path starts with || . Manually fixing path: ${apk_path}"
        apk_path="${apk_path:2}"
        ;;
     *\|\|)
-       echo_warn "App path ends with || . Manually fixing path: ${apk_path}"
+       echo_warn "The app path ends with || . Manually fixing path: ${apk_path}"
        apk_path="${apk_path%??}"
        ;;
     \|*\|)
@@ -95,7 +130,7 @@ case "${test_apk_path}" in
 esac
 
 if [ ! -f "${test_apk_path}" ] ; then
-    echo_fail "App path defined but the file does not exist at path: ${test_apk_path}"
+    echo_fail "Test apk path defined but the file does not exist at path: ${test_apk_path}"
 fi
 
 if [ -z "${project_id}" ] ; then
@@ -103,14 +138,19 @@ if [ -z "${project_id}" ] ; then
 fi
 
 if [ -z "${service_account_credentials_file}" ] ; then
-    echo_fail "Service Account Credential File is not found"
+    echo_fail "Service Account Credential File is not defined"
+fi
+
+if [ ! -f "${service_account_credentials_file}" ] ; then
+    echo_fail "Service Account Credential path is defined but the file does not exist at path: ${service_account_credentials_file}"
 fi
 
 ##### Android Deploy #####
+echo_info "Deploying Android Tests to Firebase"
 
 pushd android
 # Flutter build generates files in android/ for building the app
-flutter build apk --flavor dev --dart-define="FLAVOR=dev"
+flutter build apk --flavor $dev --dart-define="FLAVOR=$dev"
 ./gradlew app:assembleAndroidTest
 ./gradlew app:assembleDebug -Ptarget=$integration_test_path
 popd
@@ -118,26 +158,31 @@ popd
 # Deploy Android Tests
 gcloud auth activate-service-account --key-file=$service_account_credentials_file
 gcloud --quiet config set project $project_id
-gcloud firebase test android run --type instrumentation \
+gcloud firebase test android run --async  --type instrumentation \
   --app $apk_path\
   --test $test_apk_path\
   --timeout 2m \
   --results-dir="./"
   
 ##### iOS Deploy WIP #####
+#echo_info "Deploying iOS Tests to Firebase"
 
-flutter build ios --flavor dev --dart-define="FLAVOR=dev" $integration_test_path --release
+#flutter build ios --flavor dev --dart-define="FLAVOR=dev" $integration_test_path --release
 
-pushd ios
-xcodebuild build-for-testing \
-  -workspace Runner.xcworkspace \
-  -scheme Runner \
-  -xcconfig Flutter/Release.xcconfig \
-  -configuration Release \
-  -derivedDataPath \
-  $output -sdk iphoneos
-popd
+#pushd ios
+#xcodebuild build-for-testing \
+#  -workspace Runner.xcworkspace \
+#  -scheme Runner \
+#  -xcconfig Flutter/Release.xcconfig \
+#  -configuration Release \
+#  -derivedDataPath \
+# $output -sdk iphoneos
+#popd
 
-pushd $product
-zip -r ios_tests.zip . -i Release-iphoneos Runner_iphoneos17.0-arm64.xctestrun
-popd
+#pushd $product
+#zip -r ios_tests.zip . -i Release-iphoneos Runner_iphoneos17.0-arm64.xctestrun
+#popd
+
+#gcloud firebase test ios run --async \
+#    --test "build/ios_integ/Build/Products/dev_dev_iphoneos17.0.xctestrun" \
+#    --device model=iphone11pro,version=14.1,locale=fr_FR,orientation=portrait
