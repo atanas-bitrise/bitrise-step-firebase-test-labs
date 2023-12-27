45ce03952abb505fee1b5e388d2a5e61d5040575
Anthony Dade <145477058+AnthonyDadeWT@users.noreply.github.com>
2023-12-27 17:33:18 -0500

    fixing usage of input

--- step.sh
+++ step.sh
@@ -69,7 +69,7 @@ if [ -z "${integration_test_path}" ] ; then
     echo "The path to the integration tests you'd like to deploy is not defined"
 fi
 
-if [ $test_android == true] ; then
+if [ $test_android == "true"] ; then
     ##### Android Deployment #####
     echo "🚀 Deploying Android Tests to Firebase 🚀"
 
@@ -113,7 +113,7 @@ if [ $test_android == true] ; then
     fi
 fi
 
-if [ $test_ios == true] ; then
+if [ $test_ios == "true"] ; then
     ##### iOS Deploy WIP #####
     echo "🚀 Deploying iOS Tests to Firebase 🚀"
 
