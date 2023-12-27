ada14192fbaa4eaa0c09217c700b43f6d16d2b62
Anthony Dade <145477058+AnthonyDadeWT@users.noreply.github.com>
2023-12-27 15:47:15 -0500

    Fixing if clauses

--- step.sh
+++ step.sh
@@ -91,21 +91,21 @@ popd
 gcloud auth activate-service-account --key-file=$service_account_credentials_file
 gcloud --quiet config set project $project_id
 
-if [  -z "${BITRISE_APK_PATH}" ] ; then 
+if [ -z "${BITRISE_APK_PATH}" ] && [ -z "${build_flavor}" ] ; then 
 gcloud firebase test android run --async --type instrumentation \
-  --app $BITRISE_APK_PATH \
-  --test build/app/outputs/apk/androidTest/free/debug/app-debug-androidTest.apk \
+  --app build/app/outputs/apk/debug/app-debug.apk \
+  --test build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
   --timeout 2m \
   --results-dir="./"
 elif [ -z "${build_flavor}" ] ; then
     gcloud firebase test android run --async --type instrumentation \
-    --app build/app/outputs/flutter-apk/app-release.apk \
+    --app $BITRISE_APK_PATH \
     --test build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
     --timeout 2m \
     --results-dir="./"
 else
     gcloud firebase test android run --async --type instrumentation \
-    --app build/app/outputs/flutter-apk/app-release.apk \
+    --app $BITRISE_APK_PATH \
     --test build/app/outputs/apk/androidTest/$build_flavor/debug/app-$build_flavor-debug-androidTest.apk \
     --timeout 2m \
     --results-dir="./"
