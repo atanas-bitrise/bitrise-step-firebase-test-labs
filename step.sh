cbde45925b91e4c5652a61093bdc5bdd6fa8b7d5
Anthony Dade <145477058+AnthonyDadeWT@users.noreply.github.com>
2023-12-27 20:31:36 -0500

    Fixing .xctestrun based on build flavor

--- step.sh
+++ step.sh
@@ -158,7 +158,7 @@ if [ "${test_ios}" == "true" ] ; then
         pushd $product_path
         echo "checking folder"
         ls
-        zip -r "ios_tests.zip" "Release-$build_flavor-iphoneos" "Runner_iphoneos$deployment_target-arm64.xctestrun"
+        zip -r "ios_tests.zip" "Release-$build_flavor-iphoneos" "${build_flavor}_${build_flavor}_iphoneos$deployment_target-arm64.xctestrun"
         popd
 
         # Running this command asynchrounsly avoids wasting runtime on waiting for test results to come back
